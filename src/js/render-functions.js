8150d16620292522adc23b42d0d3ab48ae07b6fe
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2025-08-20 15:49:42 +0300

    update

---
+++ src/js/render-functions.js
@@ -0,0 +1,49 @@
+// src/js/render-functions.js
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Контейнер для галереї
+const galleryEl = document.querySelector('.gallery');
+// Елемент лоадера
+const loaderEl = document.querySelector('.loader');
+
+// Ініціалізація SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(img => {
+      return `
+        <li class="gallery-item">
+          <a href="${img.largeImageURL}">
+            <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy"/>
+          </a>
+          <div class="info">
+            <p><b>Likes:</b> ${img.likes}</p>
+            <p><b>Views:</b> ${img.views}</p>
+            <p><b>Comments:</b> ${img.comments}</p>
+            <p><b>Downloads:</b> ${img.downloads}</p>
+          </div>
+        </li>
+      `;
+    })
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh(); // оновлюємо SimpleLightbox
+}
+
+export function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+export function showLoader() {
+  loaderEl.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  loaderEl.classList.add('hidden');
+}
